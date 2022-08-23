560e200adc1af221a3a45615a042e41ec67be998
shibuya <shibuya@DESKTOP-HEQTIJJ>
2022-08-24 04:30:40 +0900

    NewServlet.javaを作成 Lesson16 課題

--- src/models/Task.java
+++ src/models/Task.java
@@ -14,19 +14,19 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAlltasks",
-        query = "SELECT m FROM task AS m ORDER BY m.id DESC"
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
     )
 })
 @Table(name = "tasks")
-public class task {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
+    /*@Column(name = "title", length = 255, nullable = false)
+    private String title;*/
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -45,13 +45,13 @@ public class task {
         this.id = id;
     }
 
-    public String getTitle() {
+    /*public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
-    }
+    }*/
 
     public String getContent() {
         return content;
